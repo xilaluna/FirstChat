542e423a04d606b1d265611138e14212da59dfd2
Xila Luna <xilaluna2@gmail.com>
2021-09-28 23:08:06 -0700

    [add] message func

--- public/index.js
+++ public/index.js
@@ -1,17 +1,47 @@
 $(document).ready(() => {
   const socket = io.connect()
 
+  let currentUser
+
   $("#create-user-btn").click((e) => {
     e.preventDefault()
-    let username = $("#username-input").val()
-    if (username.length > 0) {
-      //Emit to the server the new user
-      socket.emit("new user", username)
+    if ($("#username-input").val().length > 0) {
+      socket.emit("new user", $("#username-input").val())
+
+      //set current user to username input
+      currentUser = $("#username-input").val()
+
       $(".username-form").remove()
+      // Have the main page visible
+      $(".main-container").css("display", "flex")
     }
   })
 
+  $("#send-chat-btn").click((e) => {
+    e.preventDefault()
+    let message = $("#chat-input").val()
+    if (message.length > 0) {
+      socket.emit("new message", {
+        sender: currentUser,
+        message: message,
+      })
+      $("#chat-input").val("")
+    }
+  })
+
+  //socket listeners
   socket.on("new user", (username) => {
-    console.log(`✋ ${username} has joined the chat! ✋`)
+    console.log(`${username} has joined the chat`)
+    // Add the new user to the online users div
+    $(".users-online").append(`<div class="user-online">${username}</div>`)
+  })
+
+  socket.on("new message", (data) => {
+    console.log(`📥 ${data.sender}: ${data.message}`)
+    $(".message-container").append(`
+    <div class="message">
+      <p class="message-user">${data.sender}: </p>
+      <p class="message-text">${data.message}</p>
+    </div>`)
   })
 })
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,7 +1,13 @@
 module.exports = (io, socket) => {
   // Listen for "new user" socket emits
+
   socket.on("new user", (username) => {
     console.log(`✋ ${username} has joined the chat! ✋`)
     io.emit("new user", username)
+  })
+
+  socket.on("new message", (data) => {
+    console.log(`📥 ${data.sender}: ${data.message}`)
+    io.emit("new message", data)
   })
 }
