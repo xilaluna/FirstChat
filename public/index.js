ebbdfdf1d44aeaa96457b941e2fc98319e7ed704
Xila Luna <xilaluna2@gmail.com>
2021-09-29 00:38:35 -0700

    Messages go to correct channel

--- public/index.js
+++ public/index.js
@@ -31,11 +31,15 @@ $(document).ready(() => {
 
   $("#send-chat-btn").click((e) => {
     e.preventDefault()
+    // Get the client's channel
+    let channel = $(".channel-current").text()
     let message = $("#chat-input").val()
     if (message.length > 0) {
       socket.emit("new message", {
         sender: currentUser,
         message: message,
+        //Send the channel over to the server
+        channel: channel,
       })
       $("#chat-input").val("")
     }
@@ -48,11 +52,16 @@ $(document).ready(() => {
   })
 
   socket.on("new message", (data) => {
-    $(".message-container").append(`
-    <div class="message">
-      <p class="message-user">${data.sender}: </p>
-      <p class="message-text">${data.message}</p>
-    </div>`)
+    //Only append the message if the user is currently in that channel
+    let currentChannel = $(".channel-current").text()
+    if (currentChannel == data.channel) {
+      $(".message-container").append(`
+        <div class="message">
+          <p class="message-user">${data.sender}: </p>
+          <p class="message-text">${data.message}</p>
+        </div>
+      `)
+    }
   })
 
   socket.on("get online users", (onlineUsers) => {
